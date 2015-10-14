e3be0a9932e7428cf59ff4d4ae8473b01733e0b2
Jacob Harding <j.harding.911@live.com>
2015-10-14 09:23:02 -0600

    Char Count and button showing. Need to still get it to stop when char count is zero

---
+++ js/animations.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".tweet-compose").click(function () {
+        $(".tweet-compose").css("height", "5em");
+        $("#tweet-controls").css("display", "inline-block");
+
+    });
+    $("#tweet-content textarea").on('keyup', function () {
+        $('#char-count').html('140' - $('#tweet-content textarea').val().length);
+        
+        if ($('tweet-content textarea').val().length >= 140) {
+            $('#tweet-control .button').prop('disabled', true);
+            
+        } else {
+            $('#tweet-controls .button').prop('disabled', false);
+        }
+        
+        if ($('#tweet-content textarea').val().length >= 130) {
+            $('#char-count').css('color', 'red');
+            
+        } else {
+            $('#char-count').css('color', 'black');
+        };
+    });
+});
